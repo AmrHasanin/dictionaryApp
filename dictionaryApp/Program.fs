d805ff83432c6b7d1b4a46732365fe1069a74784
Amr-Dawood <68282190+Amr-Dawood@users.noreply.github.com>
2024-12-15 19:45:23 +0200

    k

--- dictionaryApp/Program.fs
+++ dictionaryApp/Program.fs
@@ -7,8 +7,8 @@ do
     let form = new Form(Text = "F# Windows Forms App", Width = 400, Height = 300)
 
     // Create a button and set its properties
-    let button = new Button(Text = "I Love Shahd mooooot", Left = 150, Top = 100)
-    button.Click.Add(fun _ -> MessageBox.Show("Bthaaaaaaaaaaazr :D <3 <3  <3 <3 <3") |> ignore)
+    let button = new Button(Text = "Click Me", Left = 150, Top = 100)
+    button.Click.Add(fun _ -> MessageBox.Show("Hello from F#") |> ignore)
 
     // Add the button to the form's controls
     form.Controls.Add(button)
